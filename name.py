4c5e402d181ca55f7e182d4c2e0b47d13a23d2e5
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-23 15:51:11 -0500

    Utilizando os métodos upper e lower

--- name.py
+++ name.py
@@ -1,3 +1,7 @@
 name: str = input('Digite seu nome: ')
 print(name.title())
-# title() transforma todas as primeiras letras em maiúsculas... eu acho
+# title() transforma todas as primeiras letras em maiúsculas
+print(name.upper())
+# upper() transforma todas as letras em maiúsculas
+print(name.lower())
+# lower() transforma todas as letras em minúculas

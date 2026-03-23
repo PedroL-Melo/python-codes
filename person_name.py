60f69185a9b2a56442ce9b497e9d2d8eee423e7a
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-23 17:26:36 -0500

    citação

---
+++ person_name.py
@@ -0,0 +1,4 @@
+name = input('Informe seu nome: ')
+print(name.upper())
+print(name.lower())
+print(name.title())
---
+++ quote.py
@@ -0,0 +1,2 @@
+quote = 'Albert Einstein certa vez disse: “Uma pessoa que nunca cometeu um erro jamais tentou nada novo.”'
+print(quote)

26f7f003e04523a06217e83ce21694117e595229
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-23 17:19:08 -0500

    apostrofo

---
+++ apostrophe.py
@@ -0,0 +1,3 @@
+# text = 'One of Python's strengths is its diverse community.' - erro no python's (forma correta abaixo)
+text = 'One of Python\'s strengths is its diverse community.'# ou aspas duplas
+

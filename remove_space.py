a2ca0833e66125108be427dbdf0d0889a301f0e2
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-23 17:09:19 -0500

    Removendo todos os espaços

--- remove_space.py
+++ remove_space.py
@@ -1,5 +1,8 @@
 text = input('informe uma frase: ')
-print(text.rstrip())
-# para remover permanentemente é nescessário utilizar o método direto na variavel
-text = text.rstrip()
-print(text)
+print(text.rstrip())# remove do lado direito
+# para remover permanentemente é nescessário utilizar o metodo direto na variavel
+# text = text.rstrip()
+# print(text)
+print(text.lstrip())# remove do lado esquerdo
+print(text.strip())# remove de todos os lados
+print(text.replace(' ', '' ))# remove todos os espaços

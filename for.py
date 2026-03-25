65cdbc8b0aac5bb7c5a34f148c982e9e37060e26
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-24 21:03:36 -0500

    Iniciando manipulação de laços de repetição com o for

---
+++ for.py
@@ -0,0 +1,4 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() +', isso foi um bom truque!')
+    print('Mal posso esperar para o seu próximo truque, '+ magician.title() +'.\n')

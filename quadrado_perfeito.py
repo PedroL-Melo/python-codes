a6a456bd3ce58f8f9b568f4951b1049bec6e02dc
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-24 21:36:23 -0500

    tal da list comprehension

--- quadrado_perfeito.py
+++ quadrado_perfeito.py
@@ -2,3 +2,8 @@ list = []
 for values in range(1, 11):
     list.append(values ** 2)
 print(list)
+
+#ou pode usar as "list comprehension"
+
+list2 = [values**2 for values in range(1, 11)]
+print(list2)

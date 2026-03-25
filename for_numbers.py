ba4cce48250623732f19667d01203eebe6eccddc
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-24 21:22:11 -0500

    Mais códigos

---
+++ for_numbers.py
@@ -0,0 +1,10 @@
+for value in range(1, 6):
+    print(value)
+
+#range para criar listas
+numbers = list(range(1, 11))
+print(numbers)
+
+#pular números com range
+even_numbers = list(range(2, 11, 2))# começa no 2 e vai somando 2 até que chege no 10
+print(even_numbers)
---
+++ pizza.py
@@ -0,0 +1,4 @@
+pizzas = ['calabresa', 'bacon', 'queijo']
+for pizza in pizzas:
+    print('Eu gosto de pizza de '+ pizza +'!\n')
+print('Eu realmente gosto de pizza!')

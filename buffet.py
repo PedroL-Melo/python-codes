7c85c84e221eb4ac0220c5071ce2487ade6cebe2
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-31 21:14:29 -0500

    buffet

---
+++ buffet.py
@@ -0,0 +1,7 @@
+pratos = ('capelete', 'sushi', 'tacos', 'seviche', 'caviar')
+for prato in pratos:
+    print(prato)
+print('\n')
+pratos = ('filé', 'sushi', 'camarão', 'seviche', 'caviar')
+for prato in pratos:
+    print(prato)

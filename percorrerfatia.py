16b63bef5844872c0504fe5888332388b3daa835
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-31 20:46:56 -0500

    modificando de acordo com os exercícios

--- percorrerfatia.py
+++ percorrerfatia.py
@@ -1,3 +1,9 @@
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
 for player in players[:3]:
     print(player.title())
+print('\n')
+for player in players[1:4]:
+    print(player.title())
+print('\n')
+for player in players[2:]:
+    print(player.title())

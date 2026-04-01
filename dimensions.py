16086c9293ed86a12e94729c80dabfedbacbbe39
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-31 20:53:43 -0500

    percorrendo os valores da tupla

--- dimensions.py
+++ dimensions.py
@@ -1,3 +1,5 @@
-dimension = (500, 20)# não é possível modificar as tuplas
-print(dimension[0])
-print(dimension[1])
+dimensions = (500, 20)# não é possível modificar as tuplas
+print(dimensions[0])
+print(dimensions[1])
+for dimension in dimensions:
+    print(dimension)

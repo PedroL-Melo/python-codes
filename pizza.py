345af94317f66062f814ef16740140722e4ecc3a
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-31 20:50:25 -0500

    modificando pizzas de acordo com os exercícios

--- pizza.py
+++ pizza.py
@@ -2,3 +2,8 @@ pizzas = ['calabresa', 'bacon', 'queijo']
 for pizza in pizzas:
     print('Eu gosto de pizza de '+ pizza +'!\n')
 print('Eu realmente gosto de pizza!')
+new_pizzas = pizzas[:]
+new_pizzas.append('peperoni')
+pizzas.append('pistache')
+print(pizzas)
+print(new_pizzas)

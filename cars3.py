56cc971ccf822345a8024967ccc61bb41d501a9d
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-03-31 21:34:32 -0500

    if de carros

---
+++ cars3.py
@@ -0,0 +1,8 @@
+car = 'subaru'
+if car == 'subaru':
+    print('O carro é um subaru')
+
+#print("Is car == 'subaru'? I predict True.")
+#print(car == 'subaru')
+#print("\nIs car == 'audi'? I predict False.")
+#print(car == 'audi')

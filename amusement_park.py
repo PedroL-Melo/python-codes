949e8bd8d2426c917786a926eec16c86201d12cc
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-04-06 16:57:09 -0500

    ajustando if de verificação de idade para entrada de parque

--- amusement_park.py
+++ amusement_park.py
@@ -1,7 +1,9 @@
 age = 19
+preco: float
 if age <= 4:
-    print('Entrada gratuita!')
+    preco = 0.00
 elif age <= 18:
-    print('Entrada: 5.00$')
+    preco = 5.00
 else:
-    print('Entrada: 10.00$')
+    preco = 10.00
+print('O valor da sua entrada é R${}' .format(preco))

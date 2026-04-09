8c9eb41b0787cbfe0ad5aa0065ec4688d0c92a9e
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-04-09 12:26:43 -0500

    Ola admin

---
+++ hello_admin.py
@@ -0,0 +1,4 @@
+users = ['pedro', 'renato', 'estevão', 'ester', 'admin']
+for user in users:
+    if user == 'admin': print('Olá admin, oq voce gostaria de ver hoje')
+    else: print('Seja bem vindo {}'.format(user))

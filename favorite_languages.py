1359cf3ecce3944315d8649a4a776fdf275599ce
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-05-12 17:45:44 -0500

    Dicionário de linguagens favoritas

---
+++ favorite_languages.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+
+for name, language in favorite_languages.items():
+    print("A linguage favorita de " + name.title() + " é " + language.title() + ".")

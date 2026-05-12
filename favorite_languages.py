c21e98b598e2b62056923939038c57a759bc8300
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-05-12 18:49:26 -0500

    Arrumando tabulaçãp

--- favorite_languages.py
+++ favorite_languages.py
@@ -1,10 +1,10 @@
 favorite_languages = { #a ordem é key, value
-#chave: valor
-'jen': 'python',
-'sarah': 'c',
-'edward': 'ruby',
-'phil': 'python',
-}
+    #chave: valor
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
 
 for name, language in favorite_languages.items():
     print("A linguage favorita de " + name.title() + " é " + language.title() + ".")

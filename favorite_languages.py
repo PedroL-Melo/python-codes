d5cd0c01ab84a1e8d2cb1cfa916eea36c31e40dc
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-05-12 17:54:17 -0500

    Anotações sobre dicionários

--- favorite_languages.py
+++ favorite_languages.py
@@ -9,5 +9,7 @@ favorite_languages = { #a ordem é key, value
 for name, language in favorite_languages.items():
     print("A linguage favorita de " + name.title() + " é " + language.title() + ".")
 
+#percorrer as chaves é um comportamento padrão de um dicionário, então não é necessário usar o método keys() para obter as chaves. O código abaixo tem o mesmo resultado do código acima.
+#for name in favorite_languages:
 for names in favorite_languages.keys():
     print(names.title())
